aba7587e391ae00b9c4b4aeefa61a2e5704d3954
NiYiHao <s10024603@gmail.com>
2018-06-28 10:59:19 +0800

    shape_button圖案

---
+++ app/src/main/java/com/example/administrator/shape_1/shapeActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.shape_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class shapeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_shape );
+    }
+}
